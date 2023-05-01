9f6760d20e2c3615c1b1c65de7a63c4990e3bddb
minhphan46 <21522345@gm.uit.edu.vn>
2023-05-02 05:53:31 +0700

    add bottom bar

---
+++ app/src/main/java/com/example/fimae/fragments/ProfileFragment.java
@@ -0,0 +1,22 @@
+package com.example.fimae.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.fimae.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
+    }
+}
