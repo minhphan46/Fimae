657eaad01eefc6a0bac6289c21aa6352d52a312b
nhao2003 <nhathaodev@gmail.com>
2023-05-22 21:21:23 +0700

    Delete unnecessary files

--- app/src/main/java/com/example/fimae/activities/ChatActivity.java
+++ app/src/main/java/com/example/fimae/activities/ChatActivity.java
@@ -1,44 +1,36 @@
 package com.example.fimae.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.example.fimae.R;
+import com.example.fimae.adapters.UserAdapter;
 import com.example.fimae.databinding.ActivityChatBinding;
+import com.example.fimae.models.UserInfo;
+
+import java.util.Arrays;
 
 public class ChatActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityChatBinding binding;
-
+    private String[] items = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        UserAdapter adapter = new UserAdapter(this,
+                R.layout.user_chat, Arrays.asList(UserInfo.dummy));
+        binding.listView.setAdapter(adapter);
+        binding.listView.setOnItemClickListener((parent, view, position, id) -> {
 
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_chat);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-//        binding.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        });
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_chat);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
---
+++ app/src/main/java/com/example/fimae/activities/OnChatActivity.java
@@ -0,0 +1,14 @@
+package com.example.fimae.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.fimae.R;
+
+public class OnChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_chat);
+    }
+}
