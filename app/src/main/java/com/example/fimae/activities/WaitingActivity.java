bc8df47dd66467632b4e7b9c4292e35042dbcab4
4nh3k <21520579@gm.uit.edu.vn>
2023-07-20 11:51:54 +0700

    fix: random chat

--- app/src/main/java/com/example/fimae/activities/WaitingActivity.java
+++ app/src/main/java/com/example/fimae/activities/WaitingActivity.java
@@ -24,7 +24,6 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.fimae.ChatRandomActivity;
 import com.example.fimae.R;
 import com.example.fimae.adapters.SliderAdapter;
 import com.example.fimae.models.Calls;
