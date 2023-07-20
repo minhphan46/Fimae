ee037ab32285c5cbf35fdd8b48771ccf39a021c1
Phan Hien <hienlephan2003@gmail.com>
2023-07-20 19:12:30 +0700

    commit follow

---
+++ app/src/main/java/com/example/fimae/activities/PostContentActivity.java
@@ -0,0 +1,14 @@
+package com.example.fimae.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_content);
+    }
+}
---
+++ app/src/main/java/com/example/fimae/models/Follows.java
@@ -0,0 +1,2 @@
+package com.example.fimae.models;public class Follows {
+}
