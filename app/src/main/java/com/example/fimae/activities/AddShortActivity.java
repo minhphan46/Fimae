419ae902fcd9f4bc9b6296f735bda7544aeda9a5
minhphan46 <21522345@gm.uit.edu.vn>
2023-07-21 20:12:48 +0700

    add logo app

---
+++ app/src/main/java/com/example/fimae/activities/AddShortActivity.java
@@ -0,0 +1,14 @@
+package com.example.fimae;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddShortActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_short);
+    }
+}
