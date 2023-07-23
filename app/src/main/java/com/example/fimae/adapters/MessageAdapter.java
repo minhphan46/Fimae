744013e4230b5866826c89c353a87ec1d1d4f812
nhao2003 <nhathaodev@gmail.com>
2023-07-23 15:41:59 +0700

    refactor: onChat

--- app/src/main/java/com/example/fimae/adapters/MessageAdapter.java
+++ app/src/main/java/com/example/fimae/adapters/MessageAdapter.java
@@ -1,37 +1,24 @@
 package com.example.fimae.adapters;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.example.fimae.R;
-import com.example.fimae.activities.DetailPostActivity;
-import com.example.fimae.activities.MediaSliderActivity;
+import com.example.fimae.components.MessageView;
+import com.example.fimae.models.Fimaers;
 import com.example.fimae.models.Message;
 import com.example.fimae.repository.ChatRepository;
-import com.example.fimae.repository.PostRepository;
+import com.example.fimae.repository.FimaerRepository;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class MessageAdapter extends FirestoreAdapter {
@@ -71,18 +58,8 @@ public class MessageAdapter extends FirestoreAdapter {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        int type = getMessageType(viewType);
-        if (type == SENDER_VIEW_HOLDER) {
-            View view = LayoutInflater.from(context).inflate(R.layout.sender_message, parent, false);
-            return new OutgoingViewholder(view);
-        } else if (type == RECEIVER_VIEW_HOLDER) {
-            View view = LayoutInflater.from(context).inflate(R.layout.receiver_message, parent, false);
-            return new IncomingViewholder(view);
-        } else {
-            View view = LayoutInflater.from(context).inflate(R.layout.post_link_sender, parent, false);
-            return new PostLinkViewHolder(view);
-
-        }
+        MessageView messageView = new MessageView(context);
+        return new IncomingViewholder(messageView);
     }
 
     private int calculateGridColumns(int urlsSize) {
@@ -95,85 +72,99 @@ public class MessageAdapter extends FirestoreAdapter {
         }
     }
 
+    HashMap<String, Fimaers> fimaersHashMap = new HashMap<>();
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         Message message = getMessage(position);
-
-        if (holder instanceof OutgoingViewholder) {
-            OutgoingViewholder outgoingViewholder = (OutgoingViewholder) holder;
-            if (Message.TEXT.equals(message.getType())) {
-                outgoingViewholder.outgoingMsg.setText(message.getContent().toString());
-                outgoingViewholder.recyclerView.setVisibility(View.GONE);
-            } else if (Message.MEDIA.equals(message.getType())) {
-                ArrayList<String> urls = (ArrayList<String>) message.getContent();
-                ImageMessageAdapter imageMessageAdapter = new ImageMessageAdapter(urls);
-                imageMessageAdapter.setOnClickMediaItem(new ImageMessageAdapter.IClickMediaItem() {
-                    @Override
-                    public void onClick(String url, int position) {
-                        Intent intent = new Intent(context, MediaSliderActivity.class);
-                        intent.putExtra("urls", urls);
-                        intent.putExtra("currentIndex", position);
-                        context.startActivity(intent);
-                    }
-                });
-
-                outgoingViewholder.recyclerView.setAdapter(imageMessageAdapter);
-                int col = calculateGridColumns(urls.size());
-                GridLayoutManager gridLayoutManager = new GridLayoutManager(holder.itemView.getContext(), col);
-                outgoingViewholder.recyclerView.setLayoutManager(gridLayoutManager);
-                outgoingViewholder.outgoingMsg.setVisibility(View.GONE);
-            }
-        } else if (holder instanceof IncomingViewholder) {
-            IncomingViewholder incomingViewholder = (IncomingViewholder) holder;
-            incomingViewholder.incomingMsg.setText(message.getContent().toString());
-            if (Message.TEXT.equals(message.getType())) {
-                incomingViewholder.incomingMsg.setText(message.getContent().toString());
-                incomingViewholder.recyclerView.setVisibility(View.GONE);
-            } else if (Message.MEDIA.equals(message.getType())) {
-                ArrayList<String> urls = (ArrayList<String>) message.getContent();
-                ImageMessageAdapter imageMessageAdapter = new ImageMessageAdapter(urls);
-                imageMessageAdapter.setOnClickMediaItem(new ImageMessageAdapter.IClickMediaItem() {
-                    @Override
-                    public void onClick(String url, int position) {
-                        Intent intent = new Intent(context, MediaSliderActivity.class);
-                        intent.putExtra("urls", urls);
-                        intent.putExtra("currentIndex", position);
-                        context.startActivity(intent);
-                    }
-                });
-                incomingViewholder.recyclerView.setAdapter(imageMessageAdapter);
-                int col = calculateGridColumns(urls.size());
-                GridLayoutManager gridLayoutManager = new GridLayoutManager(holder.itemView.getContext(), col);
-                incomingViewholder.recyclerView.setLayoutManager(gridLayoutManager);
-                incomingViewholder.incomingMsg.setVisibility(View.GONE);
-            }
-        } else if (holder instanceof PostLinkViewHolder) {
-            PostLinkViewHolder postLinkViewHolder = (PostLinkViewHolder) holder;
-            if (getMessageType(position) == SENDER_POST_VIEW_HOLDER) {
-                ((PostLinkViewHolder) holder).linearLayout.setGravity(Gravity.END);
-            } else {
-                ((PostLinkViewHolder) holder).linearLayout.setGravity(Gravity.START);
-                ((PostLinkViewHolder) holder).title.setTextColor(Color.BLACK);
-                ((PostLinkViewHolder) holder).labelButton.setTextColor(Color.BLACK);
-                ((PostLinkViewHolder) holder).constraintLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.gray_400));
-            }
-
-            assert message != null;
-            PostRepository.getInstance().getPostById(message.getContent().toString()).addOnSuccessListener(post -> {
-                if (post.getPostImages() != null && !post.getPostImages().isEmpty()) {
-                    Glide.with(context).load(post.getPostImages().get(0)).into(postLinkViewHolder.imageView);
-                } else {
-                    postLinkViewHolder.imageView.setVisibility(View.GONE);
-                }
-                String content = post.getContent();
-                postLinkViewHolder.title.setText(content);
+        MessageView messageView = (MessageView) holder.itemView;
+        messageView.setMessage(message);
+        Fimaers fimaers = fimaersHashMap.get(message.getIdSender());
+        if (fimaers == null) {
+            FimaerRepository.getInstance().getFimaerById(message.getIdSender()).addOnSuccessListener(fimaers1 -> {
+                fimaersHashMap.put(message.getIdSender(), fimaers1);
+                messageView.setFimaers(fimaers1);
             });
-            postLinkViewHolder.itemView.setOnClickListener(view -> {
-                Intent intent = new Intent(context, DetailPostActivity.class);
-                intent.putExtra("id", message.getContent().toString());
-                context.startActivity(intent);
-            });
+        } else {
+            messageView.setFimaers( fimaers);
         }
+
+
+
+//        if (holder instanceof OutgoingViewholder) {
+//            OutgoingViewholder outgoingViewholder = (OutgoingViewholder) holder;
+//            if (Message.TEXT.equals(message.getType())) {
+//                outgoingViewholder.outgoingMsg.setText(message.getContent().toString());
+//                outgoingViewholder.recyclerView.setVisibility(View.GONE);
+//            } else if (Message.MEDIA.equals(message.getType())) {
+//                ArrayList<String> urls = (ArrayList<String>) message.getContent();
+//                ImageMessageAdapter imageMessageAdapter = new ImageMessageAdapter(urls);
+//                imageMessageAdapter.setOnClickMediaItem(new ImageMessageAdapter.IClickMediaItem() {
+//                    @Override
+//                    public void onClick(String url, int position) {
+//                        Intent intent = new Intent(context, MediaSliderActivity.class);
+//                        intent.putExtra("urls", urls);
+//                        intent.putExtra("currentIndex", position);
+//                        context.startActivity(intent);
+//                    }
+//                });
+//
+//                outgoingViewholder.recyclerView.setAdapter(imageMessageAdapter);
+//                int col = calculateGridColumns(urls.size());
+//                GridLayoutManager gridLayoutManager = new GridLayoutManager(holder.itemView.getContext(), col);
+//                outgoingViewholder.recyclerView.setLayoutManager(gridLayoutManager);
+//                outgoingViewholder.outgoingMsg.setVisibility(View.GONE);
+//            }
+//        } else if (holder instanceof IncomingViewholder) {
+//            IncomingViewholder incomingViewholder = (IncomingViewholder) holder;
+//            incomingViewholder.incomingMsg.setText(message.getContent().toString());
+//            if (Message.TEXT.equals(message.getType())) {
+//                incomingViewholder.incomingMsg.setText(message.getContent().toString());
+//                incomingViewholder.recyclerView.setVisibility(View.GONE);
+//            } else if (Message.MEDIA.equals(message.getType())) {
+//                ArrayList<String> urls = (ArrayList<String>) message.getContent();
+//                ImageMessageAdapter imageMessageAdapter = new ImageMessageAdapter(urls);
+//                imageMessageAdapter.setOnClickMediaItem(new ImageMessageAdapter.IClickMediaItem() {
+//                    @Override
+//                    public void onClick(String url, int position) {
+//                        Intent intent = new Intent(context, MediaSliderActivity.class);
+//                        intent.putExtra("urls", urls);
+//                        intent.putExtra("currentIndex", position);
+//                        context.startActivity(intent);
+//                    }
+//                });
+//                incomingViewholder.recyclerView.setAdapter(imageMessageAdapter);
+//                int col = calculateGridColumns(urls.size());
+//                GridLayoutManager gridLayoutManager = new GridLayoutManager(holder.itemView.getContext(), col);
+//                incomingViewholder.recyclerView.setLayoutManager(gridLayoutManager);
+//                incomingViewholder.incomingMsg.setVisibility(View.GONE);
+//            }
+//        } else if (holder instanceof PostLinkViewHolder) {
+//            PostLinkViewHolder postLinkViewHolder = (PostLinkViewHolder) holder;
+//            if (getMessageType(position) == SENDER_POST_VIEW_HOLDER) {
+//                ((PostLinkViewHolder) holder).linearLayout.setGravity(Gravity.END);
+//            } else {
+//                ((PostLinkViewHolder) holder).linearLayout.setGravity(Gravity.START);
+//                ((PostLinkViewHolder) holder).title.setTextColor(Color.BLACK);
+//                ((PostLinkViewHolder) holder).labelButton.setTextColor(Color.BLACK);
+//                ((PostLinkViewHolder) holder).constraintLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.gray_400));
+//            }
+//
+//            assert message != null;
+//            PostRepository.getInstance().getPostById(message.getContent().toString()).addOnSuccessListener(post -> {
+//                if (post.getPostImages() != null && !post.getPostImages().isEmpty()) {
+//                    Glide.with(context).load(post.getPostImages().get(0)).into(postLinkViewHolder.imageView);
+//                } else {
+//                    postLinkViewHolder.imageView.setVisibility(View.GONE);
+//                }
+//                String content = post.getContent();
+//                postLinkViewHolder.title.setText(content);
+//            });
+//            postLinkViewHolder.itemView.setOnClickListener(view -> {
+//                Intent intent = new Intent(context, DetailPostActivity.class);
+//                intent.putExtra("id", message.getContent().toString());
+//                context.startActivity(intent);
+//            });
+//        }
     }
 
 
@@ -197,47 +188,15 @@ public class MessageAdapter extends FirestoreAdapter {
     }
 
 
-    public static class OutgoingViewholder extends RecyclerView.ViewHolder {
-        TextView outgoingMsg;
-        RecyclerView recyclerView;
-
-        public OutgoingViewholder(@NonNull View itemView) {
-            super(itemView);
-            outgoingMsg = itemView.findViewById(R.id.outgoing_msg);
-            recyclerView = itemView.findViewById(R.id.list_images);
-        }
-    }
-
     public static class IncomingViewholder extends RecyclerView.ViewHolder {
-        TextView incomingMsg;
-        RecyclerView recyclerView;
-
+        MessageView messageView;
         public IncomingViewholder(@NonNull View itemView) {
             super(itemView);
-            incomingMsg = itemView.findViewById(R.id.incoming_msg);
-            recyclerView = itemView.findViewById(R.id.list_images);
+            messageView = (MessageView) itemView;
         }
     }
 
-    public static class PostLinkViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
-        TextView title;
-        TextView labelButton;
-        CardView cardView;
-        LinearLayout linearLayout;
-        ConstraintLayout constraintLayout;
 
-        public PostLinkViewHolder(@NonNull View itemView) {
-            super(itemView);
-            imageView = itemView.findViewById(R.id.post_image);
-            title = itemView.findViewById(R.id.post_message_title);
-            cardView = itemView.findViewById(R.id.post_link_card);
-            linearLayout = itemView.findViewById(R.id.post_message_linear_layout);
-            labelButton = itemView.findViewById(R.id.label_post_view);
-            constraintLayout = itemView.findViewById(R.id.post_message_layout);
-        }
-
-    }
 
     @Override
     public int getItemCount() {
